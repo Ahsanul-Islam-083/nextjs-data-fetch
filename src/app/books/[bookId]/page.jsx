1205ba8bf7b5906402324218e8a52e08c5a06bfa
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-04-19 19:06:36 +0600

    refactor: comment out unused generateStaticParams function in BookDetailsPage

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const generateStaticParams = async()=>{
-    const res  =await fetch('http://localhost:5000/books')
-    const books = await res.json();
-    return books.slice(1,3).map (book=>({bookId: book.id}));
-}
+// export const generateStaticParams = async()=>{
+//     const res  =await fetch('http://localhost:5000/books')
+//     const books = await res.json();
+//     return books.slice(1,3).map (book=>({bookId: book.id}));
+// }
 
 const BookDetailsPage = async({params}) => {
     const {bookId} =await params;
